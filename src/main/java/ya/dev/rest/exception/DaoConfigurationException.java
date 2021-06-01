9c0815fba2fcd0244ba15b5acf4f62468ce9428f
Yassine <yassine.assouale@gmail.com>
2021-06-01 16:55:53 +0200

    Creating the exception folder

---
+++ src/main/java/ya/dev/rest/exception/DaoConfigurationException.java
@@ -0,0 +1,22 @@
+package ya.dev.rest.exception;
+
+public class DaoConfigurationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6917898603307632397L;
+
+	public DaoConfigurationException(String message) {
+		super(message);
+	}
+
+	public DaoConfigurationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DaoConfigurationException(Throwable cause) {
+		super(cause);
+	}
+
+}
