d9f65d477beab6092eb2839d72a8a213906e5560
Yassine <yassine.assouale@gmail.com>
2021-06-01 16:56:31 +0200

    Creating DAO folder

---
+++ src/main/java/ya/dev/rest/dao/UserDao.java
@@ -0,0 +1,68 @@
+package ya.dev.rest.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import ya.dev.rest.exception.DaoException;
+import ya.dev.rest.model.User;
+
+public interface UserDao {
+
+	public EntityManager getEm();
+	
+	
+	/**
+	 * Get a user by id
+	 * @param id the id
+	 * @return the user
+	 * @throws DaoException
+	 */
+	User getById(Integer id) throws DaoException;
+	
+	/**
+	 * Get a user by username
+	 * @param username the username
+	 * @return the user
+	 * @throws DaoException
+	 */
+	User getByUsername(String username) throws DaoException;
+	
+	/**
+	 * Get a list of all users
+	 * @return the list of all users
+	 * @throws DaoException
+	 */
+	List<User> getAll() throws DaoException;
+	
+	/**
+	 * Get a user by username and password
+	 * @param username the username
+	 * @param password the password
+	 * @return DaoException
+	 * @throws DaoException
+	 */
+	User getByUsernameAndPassword(String username, String password) throws DaoException;
+	
+	/**
+	 * Create a new user
+	 * @param user the user to create
+	 * @throws DaoException
+	 */
+	void createUser(User user) throws DaoException;
+	
+	/**
+	 * Update a user
+	 * @param user the user to update
+	 * @throws DaoException
+	 */
+	void updateUser(User user) throws DaoException;
+	
+	/**
+	 * Delete a user
+	 * @param user the user to delete
+	 * @throws DaoException
+	 */
+	void deleteUser(User user) throws DaoException;
+
+}
